6743117bb8ac9505e6ddb25ef6a797e3c9e12fa4
Eduardo Rodríguez <80467741+eduardorgv@users.noreply.github.com>
2026-09-21 13:12:33 -0600

    generate random value on clicking

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // IBOutlet allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    // Variables for logic
+    let diceOptions  = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+    var leftDiceNumber = 0
+    var rightDiceNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageView1.image = UIImage(imageLiteralResourceName: diceOptions[leftDiceNumber])
+        diceImageView2.image = UIImage(imageLiteralResourceName: diceOptions[rightDiceNumber])
     }
 
 
+    // Generate random values between 0 and 5
+    @IBAction func rolButtonPressed(_ sender: UIButton) {
+        let randomLeftNumber = Int.random(in: 0...5);
+        let randomRightNumber = Int.random(in: 0...5);
+        
+        diceImageView1.image = UIImage(imageLiteralResourceName: diceOptions[randomLeftNumber])
+        diceImageView2.image = UIImage(imageLiteralResourceName: diceOptions[randomRightNumber])
+    }
 }
 
